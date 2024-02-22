f5372c56169581e7b9e8cca344b4d05afa92d968
Shubham Sharma <aman.kukreti@debutinfotech.com>
2024-02-22 23:47:18 +0530

    episode_03

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"
 
 //JS code
 // const heading = document.createElement("h1")
@@ -9,14 +10,48 @@ import ReactDOM from "react-dom";
 
 //React code
 // const heading = React.createElement("h1", {id: "heading"}, "Hey from react!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 // console.log(heading);
 
 //More complex struture
-const parentDiv = React.createElement("div", { id: "heading" },
-    [React.createElement("div", { class: "child1" },
-        [React.createElement("h1", {}, "Hey I ammm updated H1!"), React.createElement("h2", {}, "Hey I am H2!")]),
-    React.createElement("div", { class: "child2" },
-        [React.createElement("h1", {}, "Hey I am H1!"), React.createElement("h2", {}, "Hey I am H2!")])])
+// const parentDiv = React.createElement("div", { id: "heading" }, [
+//   React.createElement("div", { class: "child1" }, [
+//     React.createElement("h1", {}, "Hey I ammm updated H1!"),
+//     React.createElement("h2", {}, "Hey I am H2!"),
+//   ]),
+//   React.createElement("div", { class: "child2" }, [
+//     React.createElement("h1", {}, "Hey I am H1!"),
+//     React.createElement("h2", {}, "Hey I am H2!"),
+//   ]),
+// ]);
 
-root.render(parentDiv)
+//JSX syntax
+
+const JsHeading = () => {
+  return <h1 className="heading" tabIndex={1}>
+  Hey Written in JSX!
+</h1>
+};
+
+const v1 = 200;
+const v2 = 500;
+
+// React functional component Or
+//a peice of code that retures a JSX element
+const FirstComponent = () => {
+  return (
+    <div className="div">
+        {v1+v2} 
+      <JsHeading />
+      <JsHeading></JsHeading>
+      {JsHeading()}
+      <h2>Hey returned from component</h2>
+    </div>
+  );
+};
+
+// console.log("jsHeading ", jsHeading);
+
+console.log(FirstComponent);
+
+root.render(<FirstComponent />);
